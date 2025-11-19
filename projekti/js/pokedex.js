cc0c74c9c6cc81922da0e74d032a976c0c698f64
Zumra <s2401127@edu.bc.fi>
2025-11-19 12:50:50 +0200

    pokemonit

--- projekti/js/pokedex.js
+++ projekti/js/pokedex.js
@@ -1,5 +1,6 @@
 import { fetchGeneration, fetchPokemon } from "./api.js";
 import { renderPokemonList } from "./render.js";
+import { openModal } from "./modal.js";
 
 let currentPokemonList = [];
 
@@ -7,7 +8,6 @@ export function setPokemonList(list) {
   currentPokemonList = list;
 }
 
-// Hakutoiminto
 export function filterPokemon(searchText, selectedType) {
   searchText = searchText.toLowerCase();
 
@@ -23,20 +23,52 @@ export function filterPokemon(searchText, selectedType) {
 }
 
 document
-  .getElementById("generation-select")
+  .getElementById("generationSelect")
   .addEventListener("change", async (e) => {
     const gen = e.target.value;
 
-    // 1. Hae generaatio
     const speciesList = await fetchGeneration(gen);
-
-    // 2. Hae Pokémon-data
     const pokemonData = [];
+
     for (const p of speciesList) {
       const info = await fetchPokemon(p.url);
       if (info) pokemonData.push(info);
     }
 
     setPokemonList(pokemonData);
-    renderPokemonList(pokemonData);
+    renderPokemonList(pokemonData.slice(0, 20));
   });
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const defaultGen = "1";
+  const speciesList = await fetchGeneration(defaultGen);
+  const pokemonData = [];
+
+  for (const p of speciesList) {
+    const info = await fetchPokemon(p.url);
+    if (info) pokemonData.push(info);
+  }
+
+  setPokemonList(pokemonData);
+  renderPokemonList(pokemonData.slice(0, 20));
+});
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+  const searchValue = document.getElementById("searchInput").value.trim();
+  const selectedType = document.getElementById("typeFilter").value;
+  filterPokemon(searchValue, selectedType);
+});
+
+document.getElementById("searchInput").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const searchValue = e.target.value.trim();
+    const selectedType = document.getElementById("typeFilter").value;
+    filterPokemon(searchValue, selectedType);
+  }
+});
+
+document.getElementById("typeFilter").addEventListener("change", () => {
+  const searchValue = document.getElementById("searchInput").value.trim();
+  const selectedType = document.getElementById("typeFilter").value;
+  filterPokemon(searchValue, selectedType);
+});
