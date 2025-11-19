6efb1b58cd94463467acf306a5f9e1b71d7c0633
Zumra <s2401127@edu.bc.fi>
2025-11-19 10:24:35 +0200

    .

--- projekti/js/pokedex.js
+++ projekti/js/pokedex.js
@@ -1,2 +1,24 @@
 import { fetchGeneration, fetchPokemon } from "./api.js";
+import { openModal } from "./modal.js";
 import { renderPokemonList } from "./render.js";
+let currentPokemonList = [];
+
+export function setPokemonList(list) {
+  currentPokemonList = list;
+}
+
+// Hakutoiminto
+export function filterPokemon(searchText, selectedType) {
+  searchText = searchText.toLowerCase();
+
+  const filtered = currentPokemonList.filter((pokemon) => {
+    const matchesName = pokemon.name.toLowerCase().includes(searchText);
+
+    const matchesType =
+      selectedType === "" || pokemon.types.includes(selectedType);
+
+    return matchesName && matchesType;
+  });
+
+  renderPokemonList(filtered);
+}
